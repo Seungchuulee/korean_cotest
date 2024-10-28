7ea849d995033c9d137d054af4b66cbc3b2b11e9
SeungChul <156631003+Seungchuulee@users.noreply.github.com>
2024-10-28 17:09:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 11.81 ms
+메모리: 10.4 MB, 시간: 14.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 19:31:43
+2024년 10월 28일 17:09:48
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,29 @@
 from collections import deque
 
-def solution(maps):
-    answer = -1
-    n = len(maps)  # 세로 크기
-    m = len(maps[0])  # 가로 크기
-    visited = [[False]*m for _ in range(n)]
-    def bfs(x,y):
-        visited[x][y] = True
-        queue =deque([(x,y)])
-        dx = [-1,1,0,0]
-        dy = [0,0,-1,1]
-        while queue:
-            cur_x, cur_y = queue.popleft()
-            
-            for i in range(4):
-                next_x = cur_x + dx[i]
-                next_y = cur_y + dy[i]
-                
-                if 0 <= next_x < n and 0 <= next_y < m:
-                    if maps[next_x][next_y] == 1 and not visited[next_x][next_y]:
-                        maps[next_x][next_y] = maps[cur_x][cur_y]+1
-                        visited[next_x][next_y] = True
-                        queue.append((next_x,next_y))
-                        
-        return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
+def bfs(x, y, maps, visited, n, m):
+    visited[x][y] = True
+    queue = deque([(x, y)])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    return bfs(0,0)
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            
+            if 0 <= next_x < n and 0 <= next_y < m:
+                if maps[next_x][next_y] == 1 and not visited[next_x][next_y]:
+                    visited[next_x][next_y] = True
+                    maps[next_x][next_y] = maps[cur_x][cur_y] + 1
+                    queue.append((next_x, next_y))
+    
+    return maps[n-1][m-1] if maps[n-1][m-1] != 1 else -1
+
+def solution(maps):
+    n = len(maps)  # 행의 개수
+    m = len(maps[0])  # 열의 개수
+    visited = [[False] * m for _ in range(n)]  # 방문 기록 배열
+    
+    # BFS를 시작 지점 (0, 0)에서 실행
+    return bfs(0, 0, maps, visited, n, m)
