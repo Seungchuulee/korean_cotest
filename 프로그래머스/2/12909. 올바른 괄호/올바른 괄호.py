9953b5f5eb53ff8dc71434f366d88f9fdd06feed
SeungChul <156631003+Seungchuulee@users.noreply.github.com>
2024-10-30 12:26:04 +0900

    [level 2] Title: 올바른 괄호, Time: 15.06 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,30 +1,24 @@
 def solution(s):
-    answer = True
     li = list(s)
+    
     temp = []
-    
-    if li.count('(') != li.count(')'):
+    answer = True
+
+    if li.count(')') != li.count(')'):
         answer = False
-        return answer
-    elif li[0] == ')' or li[len(li)-1] == '(':
+    elif li[0] == ')' or li[-1] == '(':
         answer = False
-        return answer
-    
-    for i in range(len(li)):
-        if len(temp) == 0 and li[i] ==  ')':
-            answer = False
-            break
-        
-        if li[i] == '(':
-            temp.append(li[i])
-        elif li[i] == ')':
-            temp.pop()
-    
-        if i == len(li)-1 and len(temp) == 0:
-            break
-        if i == len(li)-1 and len(temp) > 0:
-            answer = False
-    
-    
+    else:
+        for i in li:
+            if i == '(':
+                temp.append('(')
+            else:
+                if len(temp) == 0:
+                    answer = False
+                    break
+                else:
+                    temp.pop()
+    if len(temp) != 0:
+        answer = False
 
     return answer
